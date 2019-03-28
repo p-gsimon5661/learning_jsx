6dea315e8115c7f68822f1fa915e482df3d67816
Mc Simon <mcsimon@Mcs-MacBook-Pro.local>
2019-03-28 13:13:24 -0400

    end of lesson push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+// import the react and the reactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react componenet
+const App = () => {
+    const buttonText = {text: 'Click me'};
+    const labelText = 'Enter Name:';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text" />
+            <button style={ {backgroundColor: 'blue', color: 'white'} }>
+                { buttonText.text }
+            </button>
+        </div> 
+    );
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root') 
+);
